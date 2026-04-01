e3fe571466bc480716790ccc455eacb07eeafdda
Kaviyadharshini1211 <kaviyadharshinim3@gmail.com>
2026-04-01 20:49:25 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));   // ✅ handle ALL preflight OPTIONS requests
+app.options(/.*/, cors(corsOptions));
 
 app.use(express.json());
 
